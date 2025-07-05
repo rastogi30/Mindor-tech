20e30c598ec990b89659c35ecce08d6af981e14b
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-07-05 16:40:27 +0530

    case study page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from './pages/HomePage.jsx';
 import FAQPage from './pages/FAQPage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 import CareersPage from './pages/CareersPage.jsx';
+import CaseStudiesPage from './pages/CaseStudiesPage.jsx';
+import CaseStudyDetailPage from './pages/CaseStudyDetailPage.jsx';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Route path="/faq" element={<FAQPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/careers" element={<CareersPage />} />
+        <Route path="/case-studies" element={<CaseStudiesPage />} />
+        <Route path="/case-studies/:id" element={<CaseStudyDetailPage />} />
       </Routes>
     </Router>
   );
