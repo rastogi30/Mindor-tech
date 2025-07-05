39b0972b225acbd78dae61c9e52248669d3449cd
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-07-05 17:13:13 +0530

    App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
-import FAQPage from './pages/FAQPage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 import CareersPage from './pages/CareersPage.jsx';
 import CaseStudiesPage from './pages/CaseStudiesPage.jsx';
 import CaseStudyDetailPage from './pages/CaseStudyDetailPage.jsx';
+import ServicesPage from './pages/ServicesPage.jsx';
+import ServiceDetailPage from './pages/ServiceDetailPage.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/faq" element={<FAQPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/careers" element={<CareersPage />} />
         <Route path="/case-studies" element={<CaseStudiesPage />} />
         <Route path="/case-studies/:id" element={<CaseStudyDetailPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/services/:id" element={<ServiceDetailPage />} />
       </Routes>
     </Router>
   );
