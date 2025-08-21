6f5f7131a198163cbaf78991ee95f46aca42e25d
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-08-21 17:26:38 +0530

    minor fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
