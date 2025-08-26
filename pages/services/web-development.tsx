9e02fc575113517ecbd9c8624c4c9df919e3170f
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-08-26 18:55:57 +0530

    web development service routing

---
+++ pages/services/web-development.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import WebDevelopment from '../../src/services/Webd';
+
+const WebDevelopmentPage: React.FC = () => {
+  return <WebDevelopment />;
+};
+
+export default WebDevelopmentPage;
---
+++ src/services/Webd.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '@/components/Navbar';
+
+const Webd: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Webd;
