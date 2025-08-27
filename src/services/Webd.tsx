2d2c921aa87ec4b40e3aa76eeea8705a1553077c
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-08-27 15:32:18 +0530

    mindor component added

--- src/services/Webd.tsx
+++ src/services/Webd.tsx
@@ -5,11 +5,13 @@ import Footer from '@/components/Footer';
 import FAQ from '@/components/webcomponents/faq';
 import ChooseUs from '@/components/webcomponents/chooseus';
 import Timeline from '@/components/webcomponents/timeline';
+import Mindor from '@/components/Mindor';
 
 const Webd: React.FC = () => {
   return (
     <div className=" ">
       <Navbar />
+      <Mindor variant="service" />
       <Services />
       <Timeline />
       <ChooseUs />
