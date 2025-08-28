cdb4a37a4bcd628cf97ed385504dabac13bf6da2
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-08-28 19:00:54 +0530

    blockchain route is setup

---
+++ pages/services/blockchain-development.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Block from '@/services/block';
+
+const BlockchainDevelopmentPage: React.FC = () => {
+  return <Block />;
+};
+
+export default BlockchainDevelopmentPage;
