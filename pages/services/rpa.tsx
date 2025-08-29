714d71e2b23bbbcc32aa046566ef54836da5dec1
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-08-29 18:37:13 +0530

    rpa services added

---
+++ pages/services/rpa.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import RPA from "@/services/rpa";
+
+const RPAPage: React.FC = () => {
+  return (
+    <div>
+      <RPA />
+    </div>
+  )
+}
+
+export default RPAPage;
---
+++ src/services/rpa.tsx
@@ -0,0 +1,12 @@
+import react from 'react'
+import Navbar from '@/components/Navbar';
+
+const RPA: React.FC = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default RPA
