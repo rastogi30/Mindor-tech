6cf3f52abbe85b385410de865da4e62d5e844874
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-08-31 09:59:25 +0530

    app developement route setup

---
+++ pages/services/mobile-app-development.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from '@/services/app';
+
+const MobileAppDevelopment = () => {
+  return (
+    <div>
+      <App/>
+    </div>
+  );
+};
+
+export default MobileAppDevelopment;
---
+++ src/services/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+
+const App: React.FC = () => {
+    return(
+        <div className='mt-7'>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default App;
