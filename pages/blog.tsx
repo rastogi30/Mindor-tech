27d400a4fd73f58ba5ec176b87edaf276e59d8d9
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-09-16 23:21:20 +0530

    blog grid added

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import CTASection from '@/components/CTASection';
+import BlogCard from '@/components/BlogCard';
+import BlogGrid from '@/components/BlogGrid';
 
 const Blog = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <CTASection />
+
+      {/* Push content below the fixed navbar */}
+      <main className="flex-1 pt-20">
+        <BlogCard />
+        <BlogGrid />
+        <CTASection />
+      </main>
+
       <Footer />
     </div>
   );
