4e1dfc7f0eaba041684429c30bc50677804b25c3
priyanshu-101 <priyanshuagarwal1008@gmail.com>
2025-09-17 21:29:11 +0530

    minor fix

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import CTASection from '@/components/CTASection';
-import BlogCard from '@/components/BlogCard';
+// import BlogCard from '@/components/BlogCard';
 import BlogGrid from '@/components/BlogGrid';
 import BlogPage from './blog/blog-page'
 
@@ -14,7 +14,7 @@ const Blog = () => {
       {/* Push content below the fixed navbar */}
       <main className="flex-">
         <BlogPage />
-        <BlogCard />
+        {/* <BlogCard /> */}
         <BlogGrid />
         <CTASection />
       </main>
