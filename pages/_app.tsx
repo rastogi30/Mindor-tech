b3e94979ecf90f949b63bd544ddff6b1e8ca325f
sumukhj1219 <sumukhjoshi75@gmail.com>
2025-09-17 14:57:34 +0530

    added favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,29 @@
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import '../styles/globals.css'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>Mindor.Tech</title>
-        <meta name="description" content="One Partner, Infinite Possibilites. Web, Mobile, AI & Digital Solutions to power smarter enterprises."/>
+        <meta
+          name="description"
+          content="One Partner, Infinite Possibilites. Web, Mobile, AI & Digital Solutions to power smarter enterprises."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon.png" />
+        <link rel="icon" type="" sizes="32x32" href="/assets/favicon.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
     </>
-  )
+  );
 }
